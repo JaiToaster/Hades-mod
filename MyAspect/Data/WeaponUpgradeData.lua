f0a146cfcaf7b856763dfb0fca9f47ea6095ed05
JaiToaster <158533425+JaiToaster@users.noreply.github.com>
2024-02-03 18:08:43 +0700

    Add files via upload

--- MyAspect/Data/WeaponUpgradeData.lua
+++ MyAspect/Data/WeaponUpgradeData.lua
@@ -1,14 +1,11 @@
 if not MyAspect.Config.Enabled then return end
-    table.insert(WeaponUpgradeData.SwordWeapon, "MyAspectTrait")
-	
-	WeaponUpgradeData.MyAspectTrait =
-    {
-			Costs = { 1, 1, 1, 1, 1, },
-			MaxUpgradeLevel = 5,
-			TraitName = "MyModTrait",
-			EquippedKitAnimation = "WeaponSwordAlt01FloatingIdleOff",
-			UnequippedKitAnimation = "WeaponSwordAlt01FloatingIdle",
-			BonusUnequippedKitAnimation = "WeaponSwordAlt01FloatingIdleBonus",
-			BonusEquippedKitAnimation = "WeaponSwordAlt01FloatingIdleOffBonus",
-			Image = "Codex_Portrait_SwordAlt01"
-		}
+    table.insert(WeaponUpgradeData.SwordWeapon, {
+            Costs = { 1, 1, 1, 1, 1, },
+            MaxUpgradeLevel = 5,
+            TraitName = "MyAspectTrait",
+            EquippedKitAnimation = "WeaponSwordAlt01FloatingIdleOff",
+            UnequippedKitAnimation = "WeaponSwordAlt01FloatingIdle",
+            BonusUnequippedKitAnimation = "WeaponSwordAlt01FloatingIdleBonus",
+            BonusEquippedKitAnimation = "WeaponSwordAlt01FloatingIdleOffBonus",
+            Image = "Codex_Portrait_SwordAlt01"
+        })
